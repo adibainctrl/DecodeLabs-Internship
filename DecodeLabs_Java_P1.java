a80d8c2b14e269902cf13e93bffc76b0a4c714f9
adibainctrl <adibashahab9@gmail.com>
2026-06-24 10:13:35 +0530

    Add main method with Scanner and Random initialization

---
+++ DecodeLabs_Java_P1.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class DecodeLabs_Java_P1{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    Random random= new Random();
+    final
