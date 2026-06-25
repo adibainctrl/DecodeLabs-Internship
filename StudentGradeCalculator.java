efb067a8b023f4cdfa0a04bde2ad3138654ba9d9
Adiba Shahab <adibashahab9@gmail.com>
2026-06-25 09:12:17 +0530

    Fix class name typo in StudentGradeCalculator

--- StudentGradeCalculator.java
+++ StudentGradeCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class StudenGradeCalculator{
+public class StudentGradeCalculator{
 
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
